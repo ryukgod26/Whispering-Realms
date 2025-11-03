420b9a75028a632b93db68a0f6e86ad02460349e
raviwhitehat <ekravi26@gmail.com>
2025-11-03 23:49:08 +0530

    Implement player movement and jumping mechanics with enhanced camera control

--- scripts/camera_controller.gd
+++ scripts/camera_controller.gd
@@ -1,6 +1,23 @@
 extends Node3D
 
+@export var horizontal_acceleration := 2.0
+@export var vertical_acceleration := 1.0
+@export var mouse_acceleration := 0.005
+@export var min_limit_x: float
+@export var max_limit_x: float
+
+
+func _process(delta: float) -> void:
+	var joy_dir = Input.get_vector("pan_left","pan_right","pan_up","pan_down")
+	rotate_from_vector(joy_dir * delta * Vector2(horizontal_acceleration,vertical_acceleration))
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
-		print(event.relative)
+		rotate_from_vector(event.relative * mouse_acceleration)
+
+func rotate_from_vector(v:Vector2):
+	if v.length() == 0: return
+	rotation.y -= v.x
+	rotation.x -= v.y
+	rotation.x = clamp(rotation.x,min_limit_x,max_limit_x)
+	#rotation.x = clamp(rotation.x,deg_to_rad(-90),deg_to_rad(90))
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,13 +1,46 @@
 extends CharacterBody3D
 
 @export var base_speed := 4.0
+@export var run_speed := 6.0
 var movement_input:Vector2 = Vector2.ZERO
+var run_input:Vector2 = Vector2.ZERO
+@onready var camera = $CameraController/Camera3D
 
-func _physics_process(_delta: float) -> void:
-	movement_input = Input.get_vector("move_left","move_right","move_forward","move_backward")
-	velocity = Vector3(movement_input.x,0,movement_input.y) * base_speed
-	
+@export var jump_height: float
+@export var jump_time_to_peak: float
+@export var jump_time_to_fall: float
+
+@onready var jump_velocity: float = ((2.0 * jump_height) / jump_time_to_peak )* -1.0
+@onready var jump_gravity: float = ((-2.0 * jump_height) / (jump_time_to_peak * jump_time_to_peak)) * -1.0
+@onready var fall_gravity: float = ((-2.0 * jump_height) / (jump_time_to_fall * jump_time_to_fall)) * -1.0
+
+func _physics_process(delta: float) -> void:
+	movement_input = Input.get_vector("move_left","move_right","move_forward","move_backward").rotated(-camera.global_rotation.y)
+	run_input = Input.get_vector("run_left","run_right","run_forward","run_backward").rotated(-camera.global_rotation.y)
+	#velocity = Vector3(movement_input.x,0,movement_input.y) * base_speed
+	var vel_2d = Vector2(velocity.x,velocity.z)
+	if run_input != Vector2.ZERO:
+		vel_2d += run_speed * delta * run_input
+		vel_2d = vel_2d.limit_length(run_speed)
+		velocity.x = vel_2d.x
+		velocity.z = vel_2d.y
+	elif  movement_input != Vector2.ZERO:
+		vel_2d += base_speed * delta * movement_input
+		vel_2d = vel_2d.limit_length(base_speed)
+		velocity.x = vel_2d.x
+		velocity.z = vel_2d.y
+	else:
+		vel_2d = vel_2d.move_toward(Vector2.ZERO,base_speed * delta * 4.0)
+		velocity.x = vel_2d.x
+		velocity.z = vel_2d.y
+		
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
+		velocity.y = -jump_velocity
+	var gravity = jump_gravity if velocity.y > 0.0 else fall_gravity
+	velocity.y -= gravity * delta
+	print(velocity)
 	move_and_slide()
+	
 	'''
 	const SPEED = 5.0
 	const JUMP_VELOCITY = 4.5

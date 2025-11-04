f7b93a46826dd9c53c5972507085fa5f63f86fe8
raviwhitehat <ekravi26@gmail.com>
2025-11-05 00:15:28 +0530

    feat: Implement attack and defend mechanics for character
    - Added new attack and defend actions in the player and godettte_skin scripts.
    - Updated input handling to include mouse button events for attack.
    - Refactored movement logic to accommodate defend state, adjusting speed accordingly.
    - Enhanced animation state machine to include new attack animations (Chop, Slice_Diagonal).
    - Introduced new resources for sword and shield, integrating them into the character's rig.
    - Improved animation blending for smoother transitions during attacks and defenses.

--- scripts/godettte_skin.gd
+++ scripts/godettte_skin.gd
@@ -1,5 +1,24 @@
 extends Node3D
-@onready var move_state_machine = $AnimationTree.get("parameters/playback")
+@onready var move_state_machine = $AnimationTree.get("parameters/MoveStateMachine/playback")
+@onready var attack_state_machine = $AnimationTree.get("parameters/AttackStateMachine/playback")
+
+var attacking := false
 
 func set_move_state(state: String)->void:
 	move_state_machine.travel(state)
+
+func attack():
+	if not attacking:
+		attack_state_machine.travel('Slice_Diagonal' if $SecondAttackTimer.time_left else 'Chop')
+	#	$AnimationTree.set("parameters/OneShot/request",AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
+		
+func attack_toggle(value: bool):
+	attacking = value
+
+func defend(forward: bool) ->void:
+	var tween = create_tween()
+	tween.tween_method(_defend_change,1.0 - float(forward), float(forward),0.25)
+
+func _defend_change(val: float) -> void:
+	$AnimationTree.set("parameters/ShieldBlend/blend_amount",val)
+	
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody3D
 
+@export var defend_speed := 2.0
 @export var base_speed := 4.0
 @export var run_speed := 6.0
 @export var jump_height: float
@@ -15,43 +16,18 @@ extends CharacterBody3D
 var movement_input:Vector2 = Vector2.ZERO
 var run_input:Vector2 = Vector2.ZERO
 var target_angle
-
+var defend := false:
+	set(value):
+		if not defend and value:
+			godettte_skin.defend(true)
+		if defend and not value:
+			godettte_skin.defend(false)
+		defend = value
+		
 func _physics_process(delta: float) -> void:
-	movement_input = Input.get_vector("move_left","move_right","move_forward","move_backward").rotated(-camera.global_rotation.y)
-	run_input = Input.get_vector("run_left","run_right","run_forward","run_backward").rotated(-camera.global_rotation.y)
-	#velocity = Vector3(movement_input.x,0,movement_input.y) * base_speed
-	var vel_2d = Vector2(velocity.x,velocity.z)
-	if run_input != Vector2.ZERO:
-		vel_2d += run_speed * delta * run_input
-		vel_2d = vel_2d.limit_length(run_speed)
-		velocity.x = vel_2d.x
-		velocity.z = vel_2d.y
-		godettte_skin.set_move_state("Running_A")
-		target_angle = -run_input.angle() + (PI/2 + 0 )
-		godettte_skin.rotation.y = rotate_toward(godettte_skin.rotation.y,target_angle,5*delta)
-		
-	elif  movement_input != Vector2.ZERO:
-		vel_2d += base_speed * delta * movement_input
-		vel_2d = vel_2d.limit_length(base_speed)
-		velocity.x = vel_2d.x
-		velocity.z = vel_2d.y
-		godettte_skin.set_move_state("Running_A")
-		target_angle = -movement_input.angle() + (PI/2 + 0 )
-		godettte_skin.rotation.y = rotate_toward(godettte_skin.rotation.y,target_angle,5*delta)
-	else:
-		vel_2d = vel_2d.move_toward(Vector2.ZERO,base_speed * delta * 4.0)
-		velocity.x = vel_2d.x
-		velocity.z = vel_2d.y
-		godettte_skin.set_move_state("Idle")
-		
-	if is_on_floor():
-		if Input.is_action_just_pressed("Jump"): 
-			velocity.y = -jump_velocity
-	else:
-		godettte_skin.set_move_state("Jump")
-		
-	var gravity = jump_gravity if velocity.y > 0.0 else fall_gravity
-	velocity.y -= gravity * delta
+	move_logic(delta)
+	jump_logic(delta)
+	ability_logic()
 	#print(velocity)
 	move_and_slide()
 
@@ -79,3 +55,44 @@ func _physics_process(delta: float) -> void:
 
 	move_and_slide()
 	'''
+
+
+func ability_logic() -> void: 
+	if Input.is_action_just_pressed("attack"):
+		godettte_skin.attack()
+	
+	defend =  Input.is_action_pressed("defend")
+
+func move_logic(delta) ->void:
+	movement_input = Input.get_vector("move_left","move_right","move_forward","move_backward").rotated(-camera.global_rotation.y)
+	var is_running:bool = Input.is_action_pressed("run")
+	var vel_2d = Vector2(velocity.x,velocity.z)
+
+	if  movement_input != Vector2.ZERO:
+		var speed = run_speed if is_running else base_speed
+		speed =  defend_speed if defend else speed
+		vel_2d += speed * delta * movement_input
+		vel_2d = vel_2d.limit_length(speed)
+		velocity.x = vel_2d.x
+		velocity.z = vel_2d.y
+		if is_running:
+			godettte_skin.set_move_state("Running")
+		else:
+			godettte_skin.set_move_state("Walking")
+		target_angle = -movement_input.angle() + (PI/2 + 0 )
+		godettte_skin.rotation.y = rotate_toward(godettte_skin.rotation.y,target_angle,5*delta)
+	else:
+		vel_2d = vel_2d.move_toward(Vector2.ZERO,base_speed * delta * 4.0)
+		velocity.x = vel_2d.x
+		velocity.z = vel_2d.y
+		godettte_skin.set_move_state("Idle")
+		
+		
+func jump_logic(delta) ->void:
+	if is_on_floor():
+		if Input.is_action_just_pressed("Jump"): 
+			velocity.y = -jump_velocity
+	else:
+		godettte_skin.set_move_state("Jump")
+	var gravity = jump_gravity if velocity.y > 0.0 else fall_gravity
+	velocity.y -= gravity * delta

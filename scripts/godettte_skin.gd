3f34d74c0c18924c9759679188a172bbdc40be4a
raviwhitehat <ekravi26@gmail.com>
2025-11-05 15:08:37 +0530

    Added Weapon Switch nd animation works according to the weapon now

--- scripts/godettte_skin.gd
+++ scripts/godettte_skin.gd
@@ -2,6 +2,8 @@ extends Node3D
 @onready var move_state_machine = $AnimationTree.get("parameters/MoveStateMachine/playback")
 @onready var attack_state_machine = $AnimationTree.get("parameters/AttackStateMachine/playback")
 @onready var right_hand_slot: BoneAttachment3D = $Rig/Skeleton3D/RightHandSlot
+@onready var wand: Node3D = $Rig/Skeleton3D/RightHandSlot/wand2
+@onready var sword: Node3D = $Rig/Skeleton3D/RightHandSlot/sword_1handed2
 
 var attacking := false
 
@@ -12,7 +14,7 @@ func attack():
 	if not attacking:
 		attack_state_machine.travel('Slice_Diagonal' if $SecondAttackTimer.time_left else 'Chop')
 		$AnimationTree.set("parameters/OneShot/request",AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
-		
+
 func attack_toggle(value: bool):
 	attacking = value
 
@@ -25,7 +27,12 @@ func _defend_change(val: float) -> void:
 
 func switch_weapon(weapon_active: bool):
 	if weapon_active:
-		right_hand_slot.visible = false
+		sword.show()
+		wand.hide()
 	else:
-		right_hand_slot.visible = true
-		
+		sword.hide()
+		wand.show()
+
+func cast_spell():
+	if not attacking:
+		$AnimationTree.set("parameters/SpellCastShoot/request",AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
--- scripts/player.gd
+++ scripts/player.gd
@@ -32,36 +32,13 @@ func _physics_process(delta: float) -> void:
 	#print(velocity)
 	move_and_slide()
 
-	'''
-	const SPEED = 5.0
-	const JUMP_VELOCITY = 4.5
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("Jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("move_left","move_right","move_forward","move_backward")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
-	move_and_slide()
-	'''
-
-
 func ability_logic() -> void:
 	#Handling Attack 
 	if Input.is_action_just_pressed("attack"):
-		godettte_skin.attack()
+		if weapon_active:
+			godettte_skin.attack()
+		else:
+			godettte_skin.cast_spell()
 	
 	#Handling Defend
 	defend =  Input.is_action_pressed("defend")
@@ -95,8 +72,8 @@ func move_logic(delta) ->void:
 		velocity.x = vel_2d.x
 		velocity.z = vel_2d.y
 		godettte_skin.set_move_state("Idle")
-		
-		
+
+
 func jump_logic(delta) ->void:
 	#Handling Jumnp
 	if is_on_floor():

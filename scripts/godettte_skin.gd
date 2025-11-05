abfa9cf89f7152c26764e9bc94aba432bff53d6e
raviwhitehat <ekravi26@gmail.com>
2025-11-05 17:25:43 +0530

    Added a Squash and stretch animation for jump and also added a blink animation which  randomly blinks the eyes of player character like in real life

--- scripts/godettte_skin.gd
+++ scripts/godettte_skin.gd
@@ -1,11 +1,26 @@
 extends Node3D
+
 @onready var move_state_machine = $AnimationTree.get("parameters/MoveStateMachine/playback")
 @onready var attack_state_machine = $AnimationTree.get("parameters/AttackStateMachine/playback")
 @onready var right_hand_slot: BoneAttachment3D = $Rig/Skeleton3D/RightHandSlot
 @onready var wand: Node3D = $Rig/Skeleton3D/RightHandSlot/wand2
 @onready var sword: Node3D = $Rig/Skeleton3D/RightHandSlot/sword_1handed2
+@onready var extra_animation = $AnimationTree.get_tree_root().get_node("ExtraAnimation")
+@onready var face_material: StandardMaterial3D = $Rig/Skeleton3D/Godette_Head.get_surface_override_material(0)
 
 var attacking := false
+var squash_and_stretch := 1.0:
+	set(value):
+		squash_and_stretch = value
+		var negative = 1.0 + (1.0 - value)
+		scale = Vector3(negative,squash_and_stretch,negative)
+var rng = RandomNumberGenerator.new()
+
+const faces ={
+	'default': Vector3.ZERO,
+	'concentrate': Vector3(0.5,0,0),
+	'blink': Vector3(0,0.5,0)
+}
 
 func set_move_state(state: String)->void:
 	move_state_machine.travel(state)
@@ -33,6 +48,23 @@ func switch_weapon(weapon_active: bool):
 		sword.hide()
 		wand.show()
 
-func cast_spell():
+func cast_spell() -> void:
 	if not attacking:
-		$AnimationTree.set("parameters/SpellCastShoot/request",AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
+		extra_animation.animation = "Spellcast_Shoot"
+		$AnimationTree.set("parameters/ExtraShoot/request",AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
+
+func hit() -> void:
+	extra_animation.animation = "Hit_B"
+	$AnimationTree.set("parameters/ExtraShoot/request",AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
+	$AnimationTree.set("parameters/AttackOneShot/request",AnimationNodeOneShot.ONE_SHOT_REQUEST_ABORT)
+	attacking = false
+
+func change_face(expression) -> void:
+	face_material.uv1_offset = faces[expression]
+
+
+func _on_blink_timer_timeout() -> void:
+	change_face('blink')
+	await  get_tree().create_timer(0.2).timeout
+	change_face('default')
+	$BlinkTimer.wait_time = rng.randf_range(1.2,2.1)
--- scripts/player.gd
+++ scripts/player.gd
@@ -24,12 +24,15 @@ var defend := false:
 			godettte_skin.defend(false)
 		defend = value
 var weapon_active := false
+var speed_modifier := 1.0
+
 
 func _physics_process(delta: float) -> void:
 	move_logic(delta)
 	jump_logic(delta)
 	ability_logic()
-	#print(velocity)
+	#if Input.is_action_just_pressed("ui_accept"):
+		#hit()
 	move_and_slide()
 
 func ability_logic() -> void:
@@ -39,6 +42,7 @@ func ability_logic() -> void:
 			godettte_skin.attack()
 		else:
 			godettte_skin.cast_spell()
+			stop_movement(0.3,0.8)
 	
 	#Handling Defend
 	defend =  Input.is_action_pressed("defend")
@@ -57,8 +61,8 @@ func move_logic(delta) ->void:
 	if  movement_input != Vector2.ZERO:
 		var speed = run_speed if is_running else base_speed
 		speed =  defend_speed if defend else speed
-		vel_2d += speed * delta * movement_input
-		vel_2d = vel_2d.limit_length(speed)
+		vel_2d += speed * delta * movement_input * 8.0
+		vel_2d = vel_2d.limit_length(speed) * speed_modifier
 		velocity.x = vel_2d.x
 		velocity.z = vel_2d.y
 		if is_running:
@@ -73,13 +77,27 @@ func move_logic(delta) ->void:
 		velocity.z = vel_2d.y
 		godettte_skin.set_move_state("Idle")
 
-
 func jump_logic(delta) ->void:
 	#Handling Jumnp
 	if is_on_floor():
 		if Input.is_action_just_pressed("Jump"): 
 			velocity.y = -jump_velocity
+			do_squash_and_strecth(1.2,0.3)
 	else:
 		godettte_skin.set_move_state("Jump")
 	var gravity = jump_gravity if velocity.y > 0.0 else fall_gravity
 	velocity.y -= gravity * delta
+
+func stop_movement(start_duration: float,end_duration: float) -> void:
+	var tween = create_tween()
+	tween.tween_property(self,"speed_modifier",0.0,start_duration)
+	tween.tween_property(self,"speed_modifier",1.0,end_duration)
+
+func hit() -> void:
+	godettte_skin.hit()
+	stop_movement(0.3,0.5)
+
+func do_squash_and_strecth(value: float,duration: float = 0.1) -> void:
+	var tween = create_tween()
+	tween.tween_property(godettte_skin,"squash_and_stretch",value,duration)
+	tween.tween_property(godettte_skin,"squash_and_stretch",1.0,duration * 1.8).set_ease(Tween.EASE_OUT)

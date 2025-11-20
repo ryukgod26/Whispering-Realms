c5a9f6bfe00040f84b79a4e159c5d87523411913
raviwhitehat <ekravi26@gmail.com>
2025-11-21 00:37:41 +0530

    feat: Implement loading screen with progress bar and GUI Kit integration
    - Added new GUI Kit assets for the loading screen, including textures and SVG files.
    - Created a loading screen scene with a progress bar and text to display loading status.
    - Updated the main menu to transition to the loading screen instead of directly to the overworld.
    - Implemented threaded resource loading in the loading screen script to improve performance.
    - Removed the old loading screen assets and references from the project.

--- scripts/loading_screen.gd
+++ scripts/loading_screen.gd
@@ -1,1 +1,20 @@
 extends CanvasLayer
+
+var progress = []
+var overworld
+var scene_load_status = 0
+
+@onready var loading_progress: TextureProgressBar = $LoadingProgressBar
+@onready var progress_text: Label = $ProgressText
+
+func _ready() -> void:
+	overworld  = "res://scenes/over_world.tscn"
+	ResourceLoader.load_threaded_request(overworld)
+
+func _process(_delta: float) -> void:
+	scene_load_status = ResourceLoader.load_threaded_get_status(overworld,progress)
+	loading_progress.value = floor(progress[0] * 100)
+	progress_text.text =  str(floor(progress[0] * 100)) + "%"
+	if scene_load_status == ResourceLoader.THREAD_LOAD_LOADED:
+		var new_scene = ResourceLoader.load_threaded_get(overworld)
+		get_tree().change_scene_to_packed(new_scene)
--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/over_world.tscn")
+	get_tree().change_scene_to_file("res://scenes/loading_screen.tscn")
 
 
 func _on_option_pressed() -> void:
